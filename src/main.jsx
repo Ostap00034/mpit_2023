e5352689e3ac2675202ae16e52a7d11c3dc58405
Ostap Chichiginarov <ostap00034@gmail.com>
2023-03-15 17:34:44 +0900

    add part of registration screen

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import RegistrationScreen from './components/RegistrationScreen'
+import AuthorizationScreen from './components/AuthorizationScreen'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <RegistrationScreen />
+    <AuthorizationScreen />
   </React.StrictMode>
 )
