e8dccfbbb487916e1c7e737d0fc43064a5d8c6bb
Ostap Chichiginarov <ostap00034@gmail.com>
2023-03-15 13:31:45 +0900

    add custom markers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+import React from "react";
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 
-import './styles/index.css'
+import "./styles/index.css";
+import marker1 from "./assets/icons/marker1.png";
 
 const App = () => {
   const defaultState = {
@@ -12,12 +13,27 @@ const App = () => {
   return (
     <YMaps>
       Beer
-      <h1 className='text-lg font-serif font-bold text-rose-600'>MAP</h1>
-      <Map defaultState={defaultState} width="80vw" height="80vh">
-        <Placemark geometry={[55.684758, 37.738521]} />
+      <h1 className="text-lg font-serif font-bold text-rose-600">MAP</h1>
+      <Map
+        defaultState={defaultState}
+        modules={["layout.ImageWithContent"]}
+        width="80vw"
+        height="80vh"
+      >
+        <Placemark
+          options={{
+            balloonContent: "beer",
+            iconLayout: "default#imageWithContent",
+            iconImageHref: marker1,
+            iconImageSize: [40, 40],
+            iconImageOffset: [0, 0],
+            iconContentLayout: "<div>HUI</div>",
+          }}
+          geometry={[55.684758, 37.738521]}
+        />
       </Map>
     </YMaps>
   );
-}
+};
 
-export default App
+export default App;
